06acfa7d0b0305e5b538bfcec2ebb77056abe664
ncduy <caoduy73@gmail.com>
2021-01-07 00:50:42 +0800

    allow custom images

--- app.py
+++ app.py
@@ -1,29 +1,32 @@
-from fastai.vision.all import *
+from fastai.vision.all import load_learner, torch, PILImage
 import streamlit as st
 import os
 import time
 
-# App title
 st.title("PETS Classifier")
 
 def predict(img):
     st.image(img, use_column_width=True)
     with st.spinner('Wait for it...'): time.sleep(3)
 
-    # Load model and make prediction
     learn = load_learner('pets.pkl')
     clas, clas_idx, probs = learn.predict(img)
     prob = round(torch.max(probs).item() * 100, 2)
     
-    # Display the prediction
     st.success(f"This is {clas} with the proability of {prob}%.")
 
 
-# Test image selection
-test_images = os.listdir('images/')
-test_image = st.selectbox('Please select a test image:', test_images)
+option = st.radio('', ['Choose a test image', 'Choose your own image'])
 
-# Read and predict the image
-file_path = 'images/' + test_image
-img = PILImage.create(file_path)
-predict(img)
+if option == 'Choose a test image':
+    test_images = os.listdir('images/')
+    test_image = st.selectbox('Please select a test image:', test_images)
+    file_path = 'images/' + test_image
+    img = PILImage.create(file_path)
+    predict(img)
+
+else:
+    uploaded_file = st.file_uploader("Please upload an image", type="jpg")
+    if uploaded_file is not None:
+        img = PILImage.create(uploaded_file)
+        predict(img)
